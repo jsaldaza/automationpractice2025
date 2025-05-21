9985cff482724f8828b30790c3a33722ee9e15f7
saldaza <jsaldaza@gmail.com>
2025-05-21 17:13:54 -0500

    fix: force compatible ChromeDriver with headless options for CI

--- src/test/java/co/com/automationpractice2025/stepdefinitions/Hook.java
+++ src/test/java/co/com/automationpractice2025/stepdefinitions/Hook.java
@@ -2,13 +2,13 @@ package co.com.automationpractice2025.stepdefinitions;
 
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actors.Cast;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.thucydides.core.webdriver.ThucydidesWebDriverSupport;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hook {
 
@@ -16,12 +16,18 @@ public class Hook {
     public void setTheStage() {
         OnStage.setTheStage(Cast.ofStandardActors());
 
+        // 👇 Este es el cambio clave
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Compatible con GH Actions
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(options);
         ThucydidesWebDriverSupport.useDriver(driver);
-
-        Actor juan = OnStage.theActorCalled("Juan");
-        juan.can(BrowseTheWeb.with(driver)); // ✅ Asignar la habilidad correctamente
+        OnStage.theActorCalled("Juan").can(BrowseTheWeb.with(driver));
     }
-
 }
